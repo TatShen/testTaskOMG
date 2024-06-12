534081251211ee9c8eed21d6d49f6671f6ab192e
TatShen <shenets-97@mail.ru>
2024-06-12 23:25:20 +0200

    -add modal

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { WordsBlock } from './Components/WordsBlock/WordsBlock'
 import { LettersBlock } from './Components/LettersBlock/LettersBlock'
 import { Enter } from './Components/Enter/Enter'
 import EnterStore from './Store/EnterStore'
+import { Modal } from './Components/Modal/Modal'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
 
   return (
     <div className={styles.mainContainer}>
+    <Modal/>
       <h1>Уровень {level}</h1>
       <WordsBlock/>
       <Enter/>
---
+++ src/Components/Modal/Modal.jsx
@@ -0,0 +1,25 @@
+import ribbon from "../../../public/ribbon.svg";
+import cancel from "../../../public/cancel.svg";
+import styles from "./Modal.module.scss";
+export const Modal = () => {
+  return (
+    <div className={styles.mask}>
+      <div className={styles.modal}>
+        <div className={styles.content}>
+          <div className={styles.ribbon}>
+            <img src={ribbon} alt="ribbon"></img>
+            <p>Две вкладки <br/>с игрой?</p>
+          </div>
+          <button className={styles.cancel}>
+            <img src={cancel} alt="cancel"></img>
+          </button>
+          <p>
+            Похоже, игра открыта в нескольких вкладках браузера. Чтобы
+            продолжить играть <br/>в этой вкладке, обновите страницу.
+          </p>
+          <button className={styles.update}>Обновить</button>
+        </div>
+      </div>
+    </div>
+  );
+};
